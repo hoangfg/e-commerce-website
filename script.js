11d667dc2eb85014439c9715a6b65cec11d51466
hoangfg <121874187+hoangfg@users.noreply.github.com>
2023-05-17 18:36:56 +0700

    Page Category success

--- script.js
+++ script.js
@@ -96,9 +96,47 @@ var productBig = new Swiper('.big-image', {
 var stocks = document.querySelectorAll(".products .stock");
 for (let x = 0; x, stocks.length; x++) {
     let stock = stocks[x].dataset.stock,
-    available = stocks[x].querySelector('.qty-available').innerHTML,
-    sold = stocks[x].querySelector('.qty-sold').innerHTML,
-    percent = sold*100/stock;
+        available = stocks[x].querySelector('.qty-available').innerHTML,
+        sold = stocks[x].querySelector('.qty-sold').innerHTML,
+        percent = sold * 100 / stock;
 
     stocks[x].querySelector('.available').style.width = percent + "%"
+}
+
+
+function submitFilters() {
+    // Lấy tất cả các input checkbox trong khối lọc "Category"
+    const categoryCheckboxes = document.querySelectorAll('.filter-block#category input[type="checkbox"]');
+   
+    // Lấy tất cả các input checkbox trong khối lọc "Activity"
+    const activityCheckboxes = document.querySelectorAll('.filter-block#activity input[type="checkbox"]');
+
+    // Lấy tất cả các input checkbox trong khối lọc "Brand"
+    const brandCheckboxes = document.querySelectorAll('.filter-block#brand input[type="checkbox"]');
+
+    // Lấy giá trị của input radio trong khối lọc "Color"
+    const selectedColor = document.querySelector('.filter-block#color input[type="radio"]:checked');
+
+
+
+    // Tiến hành xử lý các giá trị đã lấy được ở trên
+    // ...
+
+    // Ví dụ: In các giá trị đã chọn ra console
+    console.log("Selected Category:", getSelectedValues(categoryCheckboxes));
+    console.log("Selected Activity:", getSelectedValues(activityCheckboxes));
+    console.log("Selected Brand:", getSelectedValues(brandCheckboxes));
+    console.log("Selected Color:", selectedColor ? selectedColor.id : null);
+
+}
+
+// Hàm trợ giúp để lấy giá trị được chọn trong một tập hợp các checkbox
+function getSelectedValues(checkboxes) {
+    const selectedValues = [];
+    checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+            selectedValues.push(checkbox.id);
+        }
+    });
+    return selectedValues;
 }
